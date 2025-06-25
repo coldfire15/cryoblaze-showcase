81ce9c713dcfe24fb8daff06b799f5cc74afcb74
coldfire <ammouri.ahmed92@gmail.com>
2025-06-25 23:42:56 +0200

    implement email service

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
 import {TranslateModule} from '@ngx-translate/core';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
-  imports: [TranslateModule],
+  imports: [TranslateModule,ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  contactForm: FormGroup;
+  isSubmitted = false;
+  isError = false;
+  isSubmitting = false;
+
+
+  constructor(private formBuilder: FormBuilder,private http: HttpClient) {
+    this.contactForm = this.formBuilder.group({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      message: new FormControl('', Validators.required),
+    })
+  }
+  submit() {
+    if(this.contactForm.invalid) return;
+    this.isSubmitting = true;
+    this.isSubmitted=false;
+    this.isError = false;
+
+    const formData = this.contactForm.value;
+    const endpoint = 'https://formspree.io/f/xvgrvwnb';
+
+    this.http.post(endpoint,formData).subscribe({
+      next: () => {
+        this.isSubmitted = true;
+        this.isSubmitting = false;
+        this.contactForm.reset()
+
+        setTimeout(() => this.isSubmitted = false, 5000);
+      },
+      error: () => {
+        this.isSubmitted = false;
+        this.isError = true;
+        setTimeout(() => this.isError = false, 5000);
+      }
+    });
+  }
 }
